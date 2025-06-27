597165d9ea1d965bfb509f36eeafb76e9e707121
PVLN CHARAN <50245131+PVLNCHRAN@users.noreply.github.com>
2025-06-27 15:20:12 +0530

    Refactor user routes and remove unused middleware

--- src/app.js
+++ src/app.js
@@ -5,8 +5,26 @@ const app = express();
 const port = 3000;
 
 // Middleware to handle all requests
-app.use((req, res) => {
-  res.send("Hello response for expres2");
+
+app.get("/user", (req, res) => {
+  res.send({
+    name: "John Doe",
+    age: 30,
+    email: "john.doe@example.com",
+    password: "John@123",
+  });
+});
+
+app.post("/user", (req, res) => {
+  res.send({
+    message: "User created successfully",
+  });
+});
+
+app.delete("/user", (req, res) => {
+  res.send({
+    message: "User deleted successfully",
+  });
 });
 
 //Listen for incoming requests
