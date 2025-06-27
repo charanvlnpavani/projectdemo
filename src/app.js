8f49240a73ec1942610012e2cffd99ad5569f223
PVLN CHARAN <50245131+PVLNCHRAN@users.noreply.github.com>
2025-06-27 21:56:53 +0530

    feat: multiple middleware router

--- src/app.js
+++ src/app.js
@@ -4,28 +4,48 @@ const app = express();
 
 const port = 3000;
 
-// Middleware to handle all requests
+//Single Middleware to handle all requests
 
-app.get("/user", (req, res) => {
-  res.send({
-    name: "John Doe",
-    age: 30,
-    email: "john.doe@example.com",
-    password: "John@123",
-  });
-});
+// app.get("/user", (req, res) => {
+//   //Router handles the request
+//   res.send({
+//     name: "John Doe",
+//     age: 30,
+//     email: "john.doe@example.com",
+//     password: "John@123",
+//   });
+// });
 
-app.post("/user", (req, res) => {
-  res.send({
-    message: "User created successfully",
-  });
-});
+// app.post("/user", (req, res) => {
+//   res.send({
+//     message: "User created successfully",
+//   });
+// });
 
-app.delete("/user", (req, res) => {
-  res.send({
-    message: "User deleted successfully",
-  });
-});
+// app.delete("/user", (req, res) => {
+//   res.send({
+//     message: "User deleted successfully",
+//   });
+// });
+
+//Multiple middlewares for the same route
+app.use(
+  "/user",
+  [
+    (req, res, next) => {
+      console.log("User route accessed");
+      // res.send("Hello from user route");
+      next();
+    },
+  ],
+  [
+    (req, res, next) => {
+      console.log("This is the second middleware for user route");
+      res.send("Hello from second middleware for user route");
+      // next();
+    },
+  ]
+);
 
 //Listen for incoming requests
 app.listen(port, () => {
