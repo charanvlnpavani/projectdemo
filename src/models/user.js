0be7140dcc94000a757e2afc78c0bc30fa55d23b
PVLN CHARAN <50245131+PVLNCHRAN@users.noreply.github.com>
2025-07-07 11:26:43 +0530

    feat: enhance user schema with detailed validation and default address

--- src/models/user.js
+++ src/models/user.js
@@ -1,22 +1,64 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+      minLength: 4,
+      maxLength: 50,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+      lowercase: true,
+      validate: {
+        validator: function (v) {
+          return /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(v);
+        },
+        message: "Invalid email format",
+      },
+    },
+    age: {
+      type: Number,
+    },
+    gender: {
+      type: String,
+      validate(v) {
+        if (!["male", "female", "other"].includes(v)) {
+          throw new Error("Invalid Gender Value - Must be male/female/other");
+        }
+        return true;
+      },
+    },
+    phoneno: {
+      type: Number,
+      required: true,
+      unique: true,
+      validate: {
+        validator: function (v) {
+          return /^\d{10}$/.test(v); // Validate that the phone number is 10 digits
+        },
+        message: "Phone number must be 10 digits",
+      },
+    },
+    address: {
+      type: String,
+      default: "Not Provided Yet - Please Update Your Address",
+    },
   },
-  password: {
-    type: String,
-  },
-  email: {
-    type: String,
-  },
-  age: {
-    type: Number,
-  },
-  gender: {
-    type: String,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
+userSchema.index({ email: 1 }, { unique: true }); // Explicitly create unique index
 
 const User = mongoose.model("User", userSchema);
 
